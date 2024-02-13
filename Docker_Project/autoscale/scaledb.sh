2fc3c29c941e86b26ae2497a16e186ceab17aa1a
HIT <hitaurora@naver.com>
2024-02-13 14:29:43 +0900

    first commit

---
+++ Docker_Project/autoscale/scaledb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+while true
+do
+    clear
+    ssh user1@211.183.3.101 'docker stats -a --no-stream --format "{{.Name}}:{{.CPUPerc}}" | grep dbcluster' | sed 's/..$//' > test.txt && ssh user1@211.183.3.102 'docker stats -a --no-stream --format "{{.Name}}:{{.CPUPerc}}" | grep dbcluster' | sed 's/..$//' >> test.txt 
+
+    # tot에 web컨테이너 개수 저장
+    tot=$(cat test.txt | grep "dbcluster" | wc -w)
+    # scale 필요 시 1, pass 시 0
+    check=0
+
+    echo "==============SCALE CHECK=============="
+    # test.txt에 있는 리소스 값으로 scale 지정
+    for list in $(cat test.txt)
+    do
+        #scale cpu 기준 값
+        max=40
+
+        #한 줄씩 컨테이너 이름, cpu 리소스 불러오기
+        cont_name=$(echo $list | cut -d":" -f1)
+        used_cpu=$(echo $list | cut -d":" -f2)
+
+        #소수점 제거 후 max와 비교
+        for rounded_used_cpu in $(printf %.0f $used_cpu); do
+            if [ "$rounded_used_cpu" -gt "$max" ]; then
+                # 40 초과인 경우
+                check=1
+                echo $(echo $cont_name | cut -c 1-15)" CPU USE: "$rounded_used_cpu"%"
+            else
+                # 40 이하인 경우
+                echo $(echo $cont_name | cut -c 1-15)" CPU USE: "$rounded_used_cpu"%"
+            fi
+        done
+    done
+    echo "-  -  -  -  -  -  -  -  -  -  -  -  -  "
+        # scale 이 필요한 경우
+        if [ $check -eq 1 ]; then
+            if [ $tot -ge 6 ]; then
+                echo "🔴 컨테이너가 6개 이상입니다. scale out을 하지 않습니다."
+            else
+                echo "🟠 scale out을 진행합니다."
+                docker service scale db_dbcluster=$(expr $tot + 1)
+            fi
+        elif [ $tot -eq 2 ]; then
+            echo "🟢 정상입니다."
+        else
+            echo "🟡 scale in을 진행합니다."
+            docker service scale db_dbcluster=2
+        fi
+    echo "======================================="
+    sleep 3
+done
